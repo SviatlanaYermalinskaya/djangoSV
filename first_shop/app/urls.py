040bedc2e5c34e7825607b583dea9bcc8bc5a5cd
Sviatlana Yermalinskaya <orimailrb@gmail.com>
2024-05-17 19:31:19 +0300

    add class-based functionality (views.py)

--- first_shop/app/urls.py
+++ first_shop/app/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    path('hello/', views.hello),
     path('', views.index, name='index'),
+    path('product/list/', views.ProductListView.as_view(), name='products_list'),
     path('product/delete/<int:pk>/', views.product_delete, name='product_delete'),
-    path('product/instance/<int:pk>/', views.product_instance, name='product_instance'),
+    path('product/instance/<int:pk>/', views.ProductDetailView.as_view(), name='product_instance'),
 ]
--- first_shop/app/views.py
+++ first_shop/app/views.py
@@ -1,18 +1,24 @@
-from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import ListView, DetailView
 
 from .models import Product
 
 
-# Create your views here.
-def hello(request):
-    return HttpResponse("Hello")
-
-
 def index(request):
-    products = Product.objects.all()
-    context = {'products': products}
-    return render(request, 'index.html', context)
+    return render(request, 'index.html')
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'products_list.html'
+    context_object_name = 'products_list'
+    ordering = '-created'
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'product_instance.html'
+    context_object_name = 'product'
 
 
 def product_delete(request, pk):
@@ -20,8 +26,8 @@ def product_delete(request, pk):
     return redirect('index')
 
 
-def product_instance(request, pk):
-    product = get_object_or_404(Product, id=pk)
-    context = {'product': product}
-    return render(request, 'product_instance.html', context)
+# def product_instance(request, pk):
+#     product = get_object_or_404(Product, id=pk)
+#     context = {'product': product}
+#     return render(request, 'product_instance.html', context)
 
