2f57776864457e324008264523e1158ca4c0946b
Sviatlana Yermalinskaya <orimailrb@gmail.com>
2024-05-21 00:27:16 +0300

    add settings for admin panel

--- first_shop/app/admin.py
+++ first_shop/app/admin.py
@@ -18,23 +18,15 @@ class ProductImageInline(admin.TabularInline):
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('title', 'price', 'quantity', 'vendor_code')
+    list_filter = ('gender', 'category')
+    search_fields = ('title__icontains', 'vendor_code')
     inlines = [ProductImageInline, ]
+    date_hierarchy = 'created'
+    fields = ['title', ('price', 'quantity', 'size'), ('vendor_code', 'category', 'gender'), 'product_description',
+              'model_description', 'delivery_description']
+    search_help_text = 'Введите название или артикул товара'
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('name', 'surname', 'email', 'phone', 'city', 'street_name')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
